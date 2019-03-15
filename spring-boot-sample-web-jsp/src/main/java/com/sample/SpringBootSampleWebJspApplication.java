d37467f25a0417a33587ef69f1296613af551786
liud <shadowfax@163.com>
2019-03-15 13:29:05 +0800

    spring-boot-web-jsp

---
+++ spring-boot-sample-web-jsp/src/main/java/com/sample/SpringBootSampleWebJspApplication.java
@@ -0,0 +1,13 @@
+package com.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSampleWebJspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSampleWebJspApplication.class, args);
+    }
+
+}
